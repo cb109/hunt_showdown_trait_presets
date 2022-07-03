f65216820b220f1da28592775a3df7429508f9b9
Christoph Buelter <buelter.christoph@gmail.com>
2022-07-03 14:44:07 +0200

    Do nothing if game is not running

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from gui import launch_gui
 def main():
     @ui_automation.skipped_by_escape_key
     def equip_selected_traits(selected_traits: list):
-        ui_automation.set_hunt_showdown_as_foreground_window()
+        if not ui_automation.set_hunt_showdown_as_foreground_window():
+            # Hunt does not seem to run.
+            return
         for trait in selected_traits:
             ui_automation.add_trait(trait["name"])
 
